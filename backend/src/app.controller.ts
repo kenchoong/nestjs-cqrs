3464a81868402ac28c816725b6d6fa130502d0a5
ken <matchartproduction@gmail.com>
2022-11-26 01:47:24 +0800

    feat/add health check url

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
-@Controller()
-export class AppController {}
+@Controller('app')
+export class AppController {
+  @Get('/health')
+  @ApiResponse({
+    status: 20,
+    description: 'health check',
+  })
+  getHello(): string {
+    return 'ok';
+  }
+}
